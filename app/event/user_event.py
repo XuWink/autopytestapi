76d8263b52f52ba366cf72ce35c4e682dff1108e
wenke.xu <2036739377@qq.com>
2025-05-18 22:48:51 +0800

    refactor(faker.py): 对用户模拟数据的生成进行了重命名

--- app/event/user_event.py
+++ app/event/user_event.py
@@ -2,7 +2,7 @@ from common.logger import logger
 from common.mysql import db
 from common.md5 import encrypt_string
 from common.redis import redis_client
-from common.generate_fake_data import generator
+from common.faker import generator
 
 def get_all_users():
     sql = f"SELECT id, username, password, telephone, role, sex, address FROM user"

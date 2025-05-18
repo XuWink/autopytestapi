2502469b8d69099e20680912f4d86fa3b79a95d6
wenke.xu <2036739377@qq.com>
2025-05-18 22:17:34 +0800

    feat(user_event): 添加了服务层

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 from flask import Blueprint
+from app.event import user_event
 
 user_bp = Blueprint('user_api', __name__) # 第一个参数是蓝图名称
 
@@ -11,13 +12,11 @@ def hello():
 @user_bp.route("/users", methods=["GET"])
 def get_all_users():
     """获取所有用户信息"""
-    sql = "SELECT * FROM user"
-    data = db.select_db(sql)
-    print("获取所有用户信息 == >> {}".format(data))
+    data = user_event.get_all_users()
     return jsonify({"code": 0, "data": data, "msg": "查询成功"})
 
 
-@user_bp.route("/users", methods=["POST"])
-def create_new_user():
-    """<UNK>"""
-    data = request.get_json()
+# @user_bp.route("/users", methods=["POST"])
+# def create_new_user():
+#     """<UNK>"""
+#     data = request.get_json()
---
+++ app/event/user_event.py
@@ -0,0 +1,10 @@
+from common.logger import logger
+from common.mysql import db
+from common.md5 import encrypt_string
+from common.redis import redis_client
+from common.generate_fake_data import generator
+
+def get_all_users():
+    sql = f"SELECT id, username, password, telephone, role, sex, address FROM user"
+    data = db.select(sql)
+    return data
